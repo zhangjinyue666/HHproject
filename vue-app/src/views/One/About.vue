e2c2f944d17f95c6b537922b103095c83f654321
Limeng <17839635543@163.com>
2020-12-16 15:06:06 +0800

    one文件提交

--- vue-app/src/views/One/About.vue
+++ vue-app/src/views/One/About.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <h1>OneOne</h1>
+<<<<<<< HEAD
+    <h1>OneOne哈哈哈</h1>
+=======
+>>>>>>> parent of 042a5ca (one文件)
   </div>
 </template>
