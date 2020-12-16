12a6122f2840c5628a8e8501fcbef4a6e1d1ff64
chenzicheng <chenzicheng_666888>
2020-12-16 15:40:30 +0800

    888888888888888888888888888

--- vue-app/src/views/Three/Home.vue
+++ vue-app/src/views/Three/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="homePage">
-        <h1>这是新建测试项目</h1>
+        <h1 class="title">这是新建测试项目</h1>
         <h1>这是Three文件夹  运行npm i 打包的项目没有自带node_modules</h1>
     </div>
 </template>
@@ -12,5 +12,8 @@
 </script>
 
 <style scoped>
-
+.title{
+    color: black;
+    border: 2px solid red;
+}
 </style>
