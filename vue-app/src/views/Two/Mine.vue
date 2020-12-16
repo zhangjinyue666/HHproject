b90f36dd0f28933c436902db53106f84dd44719b
chenzicheng <chenzicheng_666888>
2020-12-16 14:44:52 +0800

    two

--- vue-app/src/views/Two/Mine.vue
+++ vue-app/src/views/Two/Mine.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="minePage">
         <h1>这是新建测试项目  mine组件</h1>
+        <div class="top"></div>
     </div>
 </template>
 
@@ -11,5 +12,9 @@
 </script>
 
 <style scoped>
-
+.top{
+    height:100px;
+    width: 100px;
+    background-color: red;
+}
 </style>
